77c181d6f976ed00f42bc7bc4bba1e7a6e2617ad
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2026-02-17 10:20:19 -0300

    feat: expose Lenovo products via REST endpoint

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -1,1 +1,30 @@
 package controller
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/denisrodrigues-code/lenovo-scraper-api/model"
+	"github.com/denisrodrigues-code/lenovo-scraper-api/scraper"
+	"github.com/denisrodrigues-code/lenovo-scraper-api/service"
+)
+
+func LenovoHandler(w http.ResponseWriter, r *http.Request) {
+	links, _ := scraper.GetLenovoProductLinks(
+		"https://webscraper.io/test-sites/e-commerce/static/computers/laptops",
+	)
+
+	var products []model.Product
+
+	for _, link := range links {
+		product, err := scraper.GetProductDetails(link)
+		if err == nil {
+			products = append(products, *product)
+		}
+	}
+
+	service.SortByCheapest(products)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
+}
