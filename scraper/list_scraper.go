867cbf1df3a7110f196614f50c6fafa7c114bbe8
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2026-02-17 10:11:31 -0300

    feat: implement scraper to collect Lenovo product links from listing pages

--- scraper/list_scraper.go
+++ scraper/list_scraper.go
@@ -1,1 +1,32 @@
 package scraper
+
+import (
+	"strings"
+
+	"github.com/denisrodrigues-code/lenovo-scraper-api/utils"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func GetLenovoProductLinks(baseURL string) ([]string, error) {
+	client := utils.NewClient()
+	var links []string
+
+	resp, err := client.Get(baseURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	doc, _ := goquery.NewDocumentFromReader(resp.Body)
+
+	doc.Find(".thumbnail").Each(func(i int, s *goquery.Selection) {
+		title := s.Find(".title").Text()
+		if strings.Contains(strings.ToLower(title), "lenovo") {
+			href, _ := s.Find(".title").Attr("href")
+			links = append(links, "https://webscraper.io"+href)
+		}
+	})
+
+	return links, nil
+}
