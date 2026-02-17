8b025ac0be27ddc887ffbe21446dfe76fd86c9e8
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2026-02-17 10:22:50 -0300

    feat: bootstrap HTTP server and API entrypoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/denisrodrigues-code/lenovo-scraper-api/controller"
+)
+
+func main() {
+	http.HandleFunc("/lenovo", controller.LenovoHandler)
+
+	log.Println("API rodando em :8080")
+	http.ListenAndServe(":8080", nil)
+}
