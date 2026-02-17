42558e61d4f1457341274e9731fec32c6b0cf491
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2026-02-17 10:34:07 -0300

    fix: handle pagination to scrape all laptop listing pages

--- scraper/list_scraper.go
+++ scraper/list_scraper.go
@@ -1,6 +1,8 @@
 package scraper
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/denisrodrigues-code/lenovo-scraper-api/utils"
@@ -20,13 +22,34 @@ func GetLenovoProductLinks(baseURL string) ([]string, error) {
 
 	doc, _ := goquery.NewDocumentFromReader(resp.Body)
 
-	doc.Find(".thumbnail").Each(func(i int, s *goquery.Selection) {
-		title := s.Find(".title").Text()
-		if strings.Contains(strings.ToLower(title), "lenovo") {
-			href, _ := s.Find(".title").Attr("href")
-			links = append(links, "https://webscraper.io"+href)
+	lastPage := 1
+	doc.Find(".pagination li a").Each(func(i int, s *goquery.Selection) {
+		pageText := strings.TrimSpace(s.Text())
+		page, err := strconv.Atoi(pageText)
+		if err == nil && page > lastPage {
+			lastPage = page
 		}
 	})
+
+	for page := 1; page <= lastPage; page++ {
+		pageURL := fmt.Sprintf("%s?page=%d", baseURL, page)
+
+		resp, err := client.Get(pageURL)
+		if err != nil {
+			continue
+		}
+
+		doc, _ := goquery.NewDocumentFromReader(resp.Body)
+		resp.Body.Close()
+
+		doc.Find(".thumbnail").Each(func(i int, s *goquery.Selection) {
+			title := s.Find(".title").Text()
+			if strings.Contains(strings.ToLower(title), "lenovo") {
+				href, _ := s.Find(".title").Attr("href")
+				links = append(links, "https://webscraper.io"+href)
+			}
+		})
+	}
 
 	return links, nil
 }
