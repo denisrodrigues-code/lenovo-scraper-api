64e7bd0c8494ff3a66452e00df37c862a9a53a02
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2026-02-17 10:05:40 -0300

    feat: add product and storage domain models

--- model/product.go
+++ model/product.go
@@ -1,1 +1,17 @@
 package model
+
+type StorageOption struct {
+	Size  string  `json:"size"`
+	Price float64 `json:"price"`
+}
+
+type Product struct {
+	Name        string          `json:"name"`
+	Brand       string          `json:"brand"`
+	Description string          `json:"description"`
+	Rating      int             `json:"rating"`
+	Reviews     int             `json:"reviews"`
+	Image       string          `json:"image"`
+	URL         string          `json:"url"`
+	Storages    []StorageOption `json:"storages"`
+}
